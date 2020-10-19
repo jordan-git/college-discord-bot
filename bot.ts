76e4a26e864b034df4276a52747b40ceb4567493
Jordan <19174130+jordan-git@users.noreply.github.com>
2020-10-19 12:58:46 +0100

    Import only required from fs

--- bot.ts
+++ bot.ts
@@ -1,9 +1,10 @@
 import * as dotenv from 'dotenv';
-import * as fs from 'fs';
+import { readdirSync } from 'fs';
 import { Client, Collection } from 'discord.js';
 
 import { Command } from './commands/command';
 
+// Store .env variables in process.env
 dotenv.config();
 
 interface CustomClient extends Client {
@@ -22,9 +23,9 @@ export class Bot {
         this.client = new Client();
         this.client.commands = new Collection();
 
-        const commandFiles = fs
-            .readdirSync('./commands')
-            .filter((file) => file.endsWith('.cmd.ts'));
+        const commandFiles = readdirSync('./commands').filter((file) =>
+            file.endsWith('.cmd.ts')
+        );
 
         for (const file of commandFiles) {
             const { command } = require(`./commands/${file}`);
