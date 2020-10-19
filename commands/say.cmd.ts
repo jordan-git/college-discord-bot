d740a1d20df017c4be93bdc4839e5fc925c555d1
Jordan <19174130+jordan-git@users.noreply.github.com>
2020-10-19 21:51:36 +0100

    Say command

--- commands/say.cmd.ts
+++ commands/say.cmd.ts
@@ -1,34 +1,31 @@
 import { TextChannel } from 'discord.js';
 import { Command } from './command';
-import {
-    weeklySchedule,
-    todayAsString,
-    isDay,
-    getStartHour,
-    getIdFromMessage,
-} from './util';
+import { getIdFromMessage } from './util';
 
 export const command: Command = {
-    name: 'announce',
-    aliases: ['a'],
-    description: 'Announce',
+    name: 'say',
+    aliases: ['s'],
+    description: 'Say',
     async execute(message, args) {
-        if (args.length < 2) {
-            const content = '**Format:** .announce <channel> <message>';
+        // Allow only my account until I implement admin system
+        if (message.author.id !== '757569065039429655') return;
+
+        if (args.length < 2 || !getIdFromMessage(args[0])) {
+            const content = '**Format:** .say <channel (mention/id)> <message>';
             const responseMsg = await message.channel.send(content);
 
             responseMsg.delete({ timeout: 6000 });
             message.delete({ timeout: 3000 });
-        }
-
-        // Convert mention to channel ID and send message
-        if (getIdFromMessage(args[0])) {
+        } else {
+            // Convert mention to channel ID and send message
             const channel = message.client.channels.cache.get(
                 getIdFromMessage(args[0])
             );
 
             if (channel.type === 'text')
                 (channel as TextChannel).send(args.slice(1).join(' '));
+
+            message.delete();
         }
     },
 };
